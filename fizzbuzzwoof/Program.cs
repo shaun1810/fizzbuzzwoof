052ab8bf277c74bea6ef1b4f755ec571e8c76b49
shaun <shaun@SHAUNSPC>
2022-04-03 19:19:09 +0100

    added array and rules creator, added buzz word check for custom rules

--- fizzbuzzwoof/Program.cs
+++ fizzbuzzwoof/Program.cs
@@ -24,41 +24,14 @@
                 {
                     Environment.Exit(0);
                 }
-                else {
-                    Console.WriteLine("Please enter a valid option!");
-                }
-
             }
 
         }
     }
 
     public static void StandardRules()
-    {        
-        List<int> playingNumbers = new List<int>();
-        bool userSelectingNumbers = true;
-        Console.WriteLine("Please input numbers to check! input . when finished");
-        while (userSelectingNumbers == true)
-        {
-            
-            int result;
-            var input = Console.ReadLine();         
-            if (int.TryParse(input, out result) == true) {
-                playingNumbers.Add(result);
-            }
-            else
-            {
-                if (input == ".")
-                {
-                    userSelectingNumbers = false;
-                }
-                else
-                {
-                    Console.WriteLine("Please only input numbers or the stop command .!");
-                }
-            }
-        }
-        int[] numbers = playingNumbers.ToArray();
+    {
+        var numbers = ArrayCreator();
         if (numbers.Length != 0)
         {
 
@@ -78,15 +51,100 @@
                 }
             }
         }
-        else {
+        else
+        {
             Console.WriteLine("No numbers entered!");
         }
     }
-    public static void Customrules() { 
-    
-        Console.WriteLine("Please Create you own rules enter a number to divise by and a word to play to find the multiples!");
+    public static void Customrules()
+    {
+        var rules = RulesCreator();
+       
+        if (rules.Count != 0)
+        {
+            var array = ArrayCreator();
+            for (int i = 0; i == array.Length; i++)
+            {
+                foreach (KeyValuePair<int, string> rule in rules)
+                {
+                    if (rule.Key == array[i])
+                    {
+                        Console.WriteLine(rule.Value);
+                    }
+                }
+            }
+        }
+        else { Console.WriteLine("No rules entered!"); }
+    }
+
+    public static int[] ArrayCreator()
+    {
+        List<int> playingNumbers = new List<int>();
+        bool userSelectingNumbers = true;
+        Console.WriteLine("Please input numbers to check! input . when finished");
+        while (userSelectingNumbers == true)
+        {
+
+            int result;
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out result) == true)
+            {
+                playingNumbers.Add(result);
+            }
+            else
+            {
+                if (input == ".")
+                {
+                    userSelectingNumbers = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please only input numbers or the stop command .!");
+                }
+            }
+        }
+        int[] numbers = playingNumbers.ToArray();
+        return numbers;
+    }
+
+    public static List<KeyValuePair<int, string>> RulesCreator() 
+    {
+        Console.WriteLine("Please Follow instructions Input . at any time to stop creating rules!");
+        var rules = new List<KeyValuePair<int, string>>();
         bool userCreatingRules = true;
-        while (userCreatingRules)
-    
+        while (userCreatingRules == true)
+        {
+            int result;
+            Console.WriteLine("Please enter a number to find multiples of!");
+            var choice = Console.ReadLine();
+            if ((int.TryParse(choice, out result) == true))
+            {
+                bool wordSelecting = true;
+                while (wordSelecting == true)
+                {
+                    Console.WriteLine("Please input a buzz word!");
+                    var word = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(word))
+                    {
+                        rules.Add(new KeyValuePair<int, string>(result, word));
+                        wordSelecting = false;
+                    }
+                    else { Console.WriteLine("Word cannot be null!\n"); }
+                }
+            }
+            else
+            {
+                if (choice == ".")
+                {
+                    userCreatingRules = false;
+                }
+                else
+                {
+                    Console.Write("Please input a number!\n");
+                }
+            }
+
+        }
+        return rules;
     }
 }
